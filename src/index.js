75fbe39e7d91dffaf5d9578007a5ff0c608b8761
Tristan <yanggc888@163.com>
2019-08-31 15:22:11 +0800

    fix: servicer reg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './Global.style';
-import { register } from './registerServiceWorker';
 ReactDOM.render(
   <>
     <GlobalStyle />
@@ -10,4 +9,3 @@ ReactDOM.render(
   </>,
   document.getElementById('root')
 );
-register();
