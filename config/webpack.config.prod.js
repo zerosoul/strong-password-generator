669e1a893f761801f5fd91edd259206569f8cd16
Tristan <yanggc888@163.com>
2019-09-01 14:42:10 +0800

    test: sw test again

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -63,7 +63,7 @@ module.exports = merge(commonConfig, {
     new GenerateSW({
       clientsClaim: true,
       exclude: [/\.map$/, /asset-manifest\.json$/],
-      importWorkboxFrom: 'cdn',
+      importWorkboxFrom: 'local',
       navigateFallback: publicUrl + '/index.html',
       navigateFallbackBlacklist: [
         // Exclude URLs starting with /_, as they're likely an API call
