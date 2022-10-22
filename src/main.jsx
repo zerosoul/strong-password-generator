f0def25a3e4e708d0b56821d03c0eca4ad689007
Tristan Yang <yanggc888@163.com>
2022-10-22 20:18:40 +0800

    refactor: build with vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './Global.style';
 import { register } from './serviceWorker';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <>
     <GlobalStyle />
     <App />
-  </>,
-  document.getElementById('root')
+  </>
 );
 register();
