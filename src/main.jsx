03814192da6f2b7c330f3c486070d7e596a27604
Tristan Yang <yanggc888@163.com>
2022-10-23 11:15:55 +0800

    refactor: public dir

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './Global.style';
-import { register } from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,4 +10,3 @@ root.render(
     <App />
   </>
 );
-register();
